0f6886d61439e54499358d6f27b37318417c82ca
the-ben-borg <ben@thebencurve.com>
2020-05-06 12:59:35 -0400

    Implemented merge-sort algorithm to sort unordered list.

--- Python/Codes/assignment5-mergesort.py
+++ Python/Codes/assignment5-mergesort.py
@@ -1,22 +1,43 @@
-orig_list = [2, 6, 1, 10, 9, 1, 0, 91]
+test_listA = [2, 6, 1, 10, 9, 1, 0, 91]
+test_listB = [-2, -9, 9, 1, 0, 11, -12]
 
 def merge_sort (list):
-    if (len(list) <= 1):
-        return list
 
-    l = []
-    r = []
+    if len(list) > 1:
+        middle = len(list)//2
+        lft = list[:middle]
+        rgt = list[middle:]
 
-    for x in list:
-        i = list.index(x)
-        if i < (len(list))/2:
-            l.append(x)
-        else:
-            r.append(x)
+        merge_sort(lft)
+        merge_sort(rgt)
 
-    l = merge_sort(l)
-    r = merge_sort(r)
+        i = 0
+        j = 0
+        k = 0
 
-    return l + r
+        while i < len(lft) and j < len(rgt):
+            if lft[i] < rgt[j]:
+                list[k] = lft[i]
+                i = i + 1
+            else:
+                list[k] = rgt[j]
+                j = j + 1
+            k = k + 1
 
-print(merge_sort(orig_list))
+        while i < len(lft):
+            list[k] = lft[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(rgt):
+            list[k] = rgt[j]
+            j = j + 1
+            k = k + 1
+
+    return list
+
+print("Starting List: ", test_listB)
+print("Result List: ", merge_sort(test_listB))
+
+
+
