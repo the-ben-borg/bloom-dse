446e96f3387990bfc8f07c2acb3c10a556c4d040
the-ben-borg <ben@benborg.com>
2020-06-06 22:16:43 -0400

    Start R Module 6

--- linear-algebra/assignment4-8.py
+++ linear-algebra/assignment4-8.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math as math
+
 plt.style.use('ggplot')
 
 def f(x):
@@ -8,4 +9,9 @@ def f(x):
 
 x = np.linspace(-5, 5, 1001)
 
+plt.plot(x, f(x), color="red", linestyle="solid", label="$f(x)=e^-x^2$")
+
+
+plt.legend()
+plt.show()
 
