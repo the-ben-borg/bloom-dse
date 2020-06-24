fb6b141ec3b259f6b26eb92ed673d899826d547c
Ben Perkins <ben@benborg.com>
2020-06-23 21:49:07 -0400

    Work on Module 4, on creating Lists

--- JavaTwo/MakeCar/MakeCar/Car.java
+++ JavaTwo/MakeCar/MakeCar/Car.java
@@ -16,9 +16,7 @@ public abstract class Car {
 	protected String protectedStr = "protected";
 	private String privateStr = "private";
 	public abstract void fuel();
-	public static String[] VINs = new String[100];
-	public static int currentIndex = 0; 
-	
+
 	public Car(String make, String model, int year, String color) {
 	
 		super();
@@ -28,20 +26,17 @@ public abstract class Car {
 		this.color = color;
 		this.speed = 0;
 		this.mileage = 0;
-		VIN = String.valueOf((int)(Math.random()*1000));
-		for (String existingVIN: VINs) {
-			if(VIN == existingVIN) {
-				VIN = String.valueOf((int)(Math.random()*1000));
-			}
-		}
-		VINs[currentIndex] = VIN;
-		currentIndex++;
+
 	}
 	
 	public Car(String make, String model, String color, int year) {
 		
 		this(make, model, year, color);
     }
+
+    public Car(){
+		super();
+	}
     
     public String toString() {
 		return make + " " + model + " " + year + " " + color;
@@ -51,6 +46,12 @@ public abstract class Car {
         return color;
         
     }
+
+    public String getVIN(){
+		return VIN;
+	}
+
+	public static List VINs = new List();
 
     public void changeColor(String color) {
         this.color = color;
